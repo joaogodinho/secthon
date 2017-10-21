f24eafcc7cfff6455eaa436496c5869da207d762
joaogodinho <joaocfgodinho@gmail.com>
2017-10-21 13:43:01 +0100

    domxss

--- Lockup-Challenges/dom-xss/src/main/resources/plugin/DOMXSS/js/DOMXSS.js
+++ Lockup-Challenges/dom-xss/src/main/resources/plugin/DOMXSS/js/DOMXSS.js
@@ -1,5 +1,10 @@
 function displayGreeting(name) {
 	if (name != ''){
+		name = name.replace("<","");
+		name = name.replace(">", "");
+		name = name.replace("'", "");
+		name = name.replace("\"", "");
 		document.getElementById("greeting").innerHTML="Hello, " + name+ "!";
+
 	}
 }
